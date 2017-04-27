fc80829edc030bb170b957382d07b1cfb142041f
Jeefery Bin Pahat <JEEFERP@proton.com>
2017-04-27 10:05:28 +0800

    update logic

--- index.js
+++ index.js
@@ -34,62 +34,25 @@ var intentDialog = new builder.IntentDialog({recognizers: [luisRecognizer]});
 
 //Root dialog
 bot.dialog('/', intentDialog);
-//intentDialog.onDefault(builder.DialogAction.send('Sorry, I didn\'t understand that.'));
-//intentDialog.matches('Greeting', '/greetingDialog');
-//intentDialog.matches('Size', '/sizeDialog')
-//intentDialog.matches('Distance', '/distanceDialog');
-//intentDialog.matches('Life', 'lifeDialog');
+intentDialog.onDefault(builder.DialogAction.send('Sorry, I didn\'t understand that.'));
+intentDialog.matches(/\b(hi|hello|hey|howdy)\b/i, '/greetingDialog');
 
-intentDialog.matches(/\b(hi|hello|hey|howdy)\b/i, '/sayHi') //Check for greetings using regex
-    .matches('GetNews', '/topnews') //Check for LUIS intent to get news
-    //.matches('AnalyseImage', '/analyseImage') //Check for LUIS intent to analyze image
-    .onDefault(builder.DialogAction.send("Sorry, I didn't understand what you said.")); //Default message if all checks fail
-
-bot.dialog('/sayHi', function(session) {
-    session.send('Hi there!  Try saying things like "Get news in Toyko"');
-    session.endDialog();
-});
-
-/*bot.dialog('/topNews', [
+bot.dialog('/greetingDialog', [
     function (session){
         // Ask the user which category they would like
         // Choices are separated by |
-        builder.Prompts.choice(session, "Which category would you like?", "Technology|Science|Sports|Business|Entertainment|Politics|Health|World|(quit)");
-    }, function (session, results){
-        var userResponse = results.response.entity;
-        session.endDialog("You selected: " + userResponse);
-    }
-]);
-*/
-
-bot.dialog('/topnews', [
-    function (session){
-        // Ask the user which category they would like
-        // Choices are separated by |
-        builder.Prompts.choice(session, "Which category would you like?", "Technology|Science|Sports|Business|Entertainment|Politics|Health|World|(quit)");
+        builder.Prompts.choice(session, "Hi!! Welcome Proton Auto Assist!  Have a problem which you cars? Please choose your problem'", "Engine|Door|Lamp|Steering|Others|(quit)");
     }, function (session, results, next){
         // The user chose a category
         if (results.response && results.response.entity !== '(quit)') {
            //Show user that we're processing their request by sending the typing indicator
             session.sendTyping();
-            // Build the url we'll be calling to get top news
-            var url = "https://api.cognitive.microsoft.com/bing/v5.0/news/?" 
-                + "category=" + results.response.entity + "&count=10&mkt=en-US&originalImg=true";
-            // Build options for the request
-            var options = {
-                uri: url,
-                headers: {
-                    'Ocp-Apim-Subscription-Key': BINGSEARCHKEY
-                },
-                json: true // Returns the response in json
-            }
+            session.send("Searching our database");
             //Make the call
             rp(options).then(function (body){
                 // The request is successful
                 //console.log(body); // Prints the body out to the console in json format
-                //session.send("Managed to get your news.");
-                // Call SendTopNews
-                sendTopNews(session, results, body);
+                //session.send("Nothing found.");
             }).catch(function (err){
                 // An error occurred and the request failed
                 console.log(err.message);
@@ -100,44 +63,7 @@ bot.dialog('/topnews', [
             });
         } else {
             // The user choses to quit
-            session.endDialog("Ok. Mission Aborted.");
+            session.endDialog("Ok. Thank you and nice to meet you.");
         }
     }
 ]);
-
-// This is called the root dialog. It is the first point of entry for any message the bot receives
-/*bot.dialog('/', function (session) {
-    // Send 'hello world' to the user
-    session.send("Hello World");
-});*/
-
-// This function processes the results from the API call to category news and sends it as cards
-function sendTopNews(session, results, body){
-    session.send("Top news in " + results.response.entity + ": ");
-    //Show user that we're processing by sending the typing indicator
-    session.sendTyping();
-    // The value property in body contains an array of all the returned articles
-    var allArticles = body.value;
-    var cards = [];
-    // Iterate through all 10 articles returned by the API
-    for (var i = 0; i < 10; i++){
-        var article = allArticles[i];
-        // Create a card for the article and add it to the list of cards we want to send
-        cards.push(new builder.HeroCard(session)
-            .title(article.name)
-            .subtitle(article.datePublished)
-            .images([
-                //handle if thumbnail is empty
-                builder.CardImage.create(session, article.image.contentUrl)
-            ])
-            .buttons([
-                // Pressing this button opens a url to the actual article
-                builder.CardAction.openUrl(session, article.url, "Full article")
-            ]));
-    }
-    var msg = new builder.Message(session)
-        .textFormat(builder.TextFormat.xml)
-        .attachmentLayout(builder.AttachmentLayout.carousel)
-        .attachments(cards);
-    session.send(msg);
-}
